415e65b4be259c426f4b30953f42ec9aba8d7040
AriadnaNatsuki <elizabethcolastra@hotmail.es>
2026-09-21 12:24:01 +0200

    Configure Next.js for deployment

--- sugarcoach-next/next.config.mjs
+++ sugarcoach-next/next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export", //
+  basePath: "/SugarC", //
+  assetPrefix: "/SugarC/", //
+
   images: {
+    unoptimized: true, //
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "images.unsplash.com" }
